ca385ad650d0b64015f557cefddbcf58368bc55c
chiradenuwan <chiranjayadenuwan@gmail.com>
2021-02-24 20:21:01 +0530

    init commit.add dependencies for jwt,mysql and swagger

---
+++ src/test/java/lk/maharaja/pos/pos_system/PosSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.maharaja.pos.pos_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
