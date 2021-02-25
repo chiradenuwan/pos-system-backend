1b038cf91506fcb35add555df728afc5f68395d3
chiradenuwan <chiranjayadenuwan@gmail.com>
2021-02-25 23:10:10 +0530

    Add reponse interface

---
+++ src/main/java/lk/maharaja/pos/pos_system/common/alert/Alerts.java
@@ -0,0 +1,14 @@
+package lk.maharaja.pos.pos_system.common.alert;
+
+public interface Alerts {
+    String saveSuccess = "Successfully saved!";
+    String registerSuccess = "Successfully Registered!";
+    String registerFailed = "Registration Fail!";
+    String saveFailed = "Save Failed!";
+    String updateSuccess = "Update Successful!";
+    String updateFailed = "Update Failed!";
+    String removeSuccess = "Removed Successful!";
+    String removeFailed = "Remove Failed!";
+    String backendError = "Something went wrong!";
+    String usernameInUse = "Username is in use!";
+}
